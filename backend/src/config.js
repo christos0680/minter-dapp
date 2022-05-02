0384d337147286179064d2c5290d6e3e9ed77da5
christos0680 <chris@manhattanmobile.net>
2022-05-01 20:49:53 -0700

    Website Images Added

--- backend/src/config.js
+++ backend/src/config.js
@@ -134,7 +134,7 @@ const PUBLIC_MINT_START_DATE = "2022-06-25T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-06-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 2000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x7BB1c8f5479d8200D8AA0C57f7c3Ea7F79E82100"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
